db3ec2b503b4f58194cec49ad9347ed1472c5d60
Abdelrahman Zain <boody.ali.zean@gmail.com>
2025-05-04 20:53:42 +0300

    Final Commit

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -13,20 +13,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]()
+    
+    var textNode = SCNNode()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,7 +43,70 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
+        
+        if let touchLocation = touches.first?.location(in: sceneView){
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            if let hitResult = hitTestResults.first{
+                addDot(at: hitResult)
+            }
+        }
+    }
+    
+    
+    func addDot(at hitResult: ARHitTestResult){
+        let dot = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
 
+        let dotNode = SCNNode(geometry: dot)
+        
+        dotNode.position = SCNVector3(
+            x: hitResult.worldTransform.columns.3.x,
+            y: hitResult.worldTransform.columns.3.y,
+            z: hitResult.worldTransform.columns.3.z
+        )
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        dotNodes.append(dotNode)
+    }
+
+    func calculate () {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        let distance = sqrt(
+            pow(end.position.x - start.position.x , 2) +
+            pow(end.position.y - start.position.y , 2) +
+            pow(end.position.z - start.position.z , 2)
+        )
+        
+        updateText(text: String(distance), atPosition: end.position)
+         
+    }
+    
+    func updateText (text: String, atPosition position : SCNVector3){
+        
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode = SCNNode(geometry: textGeometry)
+        textNode.position = SCNVector3(position.x, position.y, position.z)
+        textNode.scale = SCNVector3(0.1, 0.1, 0.1)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+    }
+    
     // MARK: - ARSCNViewDelegate
     
 /*
